f2c4e89b68d0fd7864dc86b3a5eb43ced3c9dc77
fadlihendrawijaya <79741801+fadlihendrawijaya@users.noreply.github.com>
2021-06-04 21:39:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-// Include functions and connect to the database using PDO MySQL
+// fungsi untuk menghubungkan ke database menggunakan PDO mySQL
 include 'functions.php';
 $pdo = pdo_connect_mysql();
-// Page is set to home (home.php) by default, so when the visitor visits that will be the page they see.
+//  Halaman akan otomotis ke (home.php) secara default, jika mengunjungi halaman yang akan dilihat
 $page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'home';
-// Include and show the requested page
+// Menampikan halaman yang diminta
 include $page . '.php';
