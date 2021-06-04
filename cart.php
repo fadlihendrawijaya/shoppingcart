0d18e790b0a1133e394bf7258190d409b6043246
fadlihendrawijaya <79741801+fadlihendrawijaya@users.noreply.github.com>
2021-06-04 21:22:03 +0700

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,58 +1,58 @@
 <?php
-// If the user clicked the add to cart button on the product page we can check for the form data
+// Jika pengguna mengklik tombol tambahkan di halaman produk, dapat memeriksa data formulir
 if (isset($_POST['product_id'], $_POST['quantity']) && is_numeric($_POST['product_id']) && is_numeric($_POST['quantity'])) {
-  // Set the post variables so we easily identify them, also make sure they are integer
+  // Mengatur variabel post agar mudah mengidentifikasinya, pastikan bilangan bulat
   $product_id = (int)$_POST['product_id'];
   $quantity = (int)$_POST['quantity'];
-  // Prepare the SQL statement, we basically are checking if the product exists in our database
+  // Untuk memeriksa apakah produk ada di database
   $stmt = $pdo->prepare('SELECT * FROM products WHERE id = ?');
   $stmt->execute([$_POST['product_id']]);
-  // Fetch the product from the database and return the result as an Array
+  // Mengambil data produk dari database untuk mengembalikan hasilnya menggunakan Array
   $product = $stmt->fetch(PDO::FETCH_ASSOC);
-  // Check if the product exists (array is not empty)
+  // Memeriksa apakah produk ada (array tidak kosong)
   if ($product && $quantity > 0) {
-    // Product exists in database, now we can create/update the session variable for the cart
+    // Jika produk ada di database, lalu dapat membuat/memperbarui variabel
     if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
       if (array_key_exists($product_id, $_SESSION['cart'])) {
-        // Product exists in cart so just update the quanity
+        // Jika produk ada di keranjang lalu memperbarui jumlahnya
         $_SESSION['cart'][$product_id] += $quantity;
       } else {
-        // Product is not in cart so add it
+        // Jika produk tidak ada, untuk menambahkannya
         $_SESSION['cart'][$product_id] = $quantity;
       }
     } else {
-      // There are no products in cart, this will add the first product to cart
+      // Jika tidak ada produk, kemudian menambahkannya produk pertama
       $_SESSION['cart'] = array($product_id => $quantity);
     }
   }
-  // Prevent form resubmission...
+  // Untuk mencegah pengiriman ulang formulir...
   header('location: index.php?page=cart');
   exit;
 }
-// Remove product from cart, check for the URL param "remove", this is the product id, make sure it's a number and check if it's in the cart
+// Jika menghapus produk dari troli, memeriksa URL "hapus", id produk, pastikan juga nomornya apakah ada di troli
 if (isset($_GET['remove']) && is_numeric($_GET['remove']) && isset($_SESSION['cart']) && isset($_SESSION['cart'][$_GET['remove']])) {
-  // Remove the product from the shopping cart
+  // Menghapus produk dari shopping cart
   unset($_SESSION['cart'][$_GET['remove']]);
 }
-// Update product quantities in cart if the user clicks the "Update" button on the shopping cart page
+// Memperbarui jumlah produk jika pengguna mengklik tombol "Perbarui" di halaman belanja
 if (isset($_POST['update']) && isset($_SESSION['cart'])) {
-  // Loop through the post data so we can update the quantities for every product in cart
+  // Jika mengulangi data maka dapat memperbarui jumlah untuk setiap produk
   foreach ($_POST as $k => $v) {
     if (strpos($k, 'quantity') !== false && is_numeric($v)) {
       $id = str_replace('quantity-', '', $k);
       $quantity = (int)$v;
-      // Always do checks and validation
+      // Untuk melakukan pengecekan dan validasi
       if (is_numeric($id) && isset($_SESSION['cart'][$id]) && $quantity > 0) {
-        // Update new quantity
+        // Memperbarui jumlah
         $_SESSION['cart'][$id] = $quantity;
       }
     }
   }
-  // Prevent form resubmission...
+  // Untuk mencengah pengiriman ulang formulir...
   header('location: index.php?page=cart');
   exit;
 }
-// Send the user to the place order page if they click the Place Order button, also the cart should not be empty
+// Untuk pengguna menuju ke halaman pemesanan jika mengklik tombol Tempatkan Pesanan, tidak boleh kosong
 if (isset($_POST['placeorder']) && isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
   $stmt = $pdo->prepare('INSERT INTO `orders` (`productID`, `accountsID`, `date_created`, `jumlah`, `total`) VALUES (?,?,?,?,?)');
 
@@ -77,21 +77,21 @@ if (isset($_POST['placeorder']) && isset($_SESSION['cart']) && !empty($_SESSION[
   header('Location: index.php?page=placeorder');
   exit;
 }
-// Check the session variable for products in cart
+// Periksa variabel produk
 $products_in_cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
 $products = array();
 $subtotal = 0.00;
-// If there are products in cart
+// Jika ada produk di keranjang
 if ($products_in_cart) {
-  // There are products in the cart so we need to select those products from the database
-  // Products in cart array to question mark string array, we need the SQL statement to include IN (?,?,?,...etc)
+  // Jika ada produk di troli, lalu perlu memilih produk tersebut dari database
+  // Jika produk dalam array, string, lalu memerlukan sql untuk menambahkannya
   $array_to_question_marks = implode(',', array_fill(0, count($products_in_cart), '?'));
   $stmt = $pdo->prepare('SELECT * FROM products WHERE id IN (' . $array_to_question_marks . ')');
-  // We only need the array keys, not the values, the keys are the id's of the products
+  // Hanya membutuhkan kunci array, id produk
   $stmt->execute(array_keys($products_in_cart));
-  // Fetch the products from the database and return the result as an Array
+  // Untuk mengambil data dari database, mengembalikan hasilnya sebagai Array
   $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  // Calculate the subtotal
+  // Menghitung jumlah 
   foreach ($products as $product) {
     $subtotal += (float)$product['price'] * (int)$products_in_cart[$product['id']];
   }
